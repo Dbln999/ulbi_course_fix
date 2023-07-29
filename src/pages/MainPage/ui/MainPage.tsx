0b973104cda4a46a53cd1237975c391885939b70
danii <daniil3221337@gmail.com>
2023-07-29 16:19:10 +0300

    pre commit

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
-    const [value, setValue] = useState('');
-
-    const onChange = (value:string) => {
-        setValue(value);
-    };
-
     return (
         <div>
             {t('Главная страница')}
-            <Counter />
         </div>
     );
 };
